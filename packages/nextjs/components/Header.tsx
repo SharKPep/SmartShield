38bd516df5d26e46cc5680d60ca1be0a303c0af4
liqwang <liqwang2@gmail.com>
2025-05-18 01:37:50 +0800

    feat: add `CircleStackIcon` on Pool button

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { hardhat } from "viem/chains";
-import { Bars3Icon, BugAntIcon, CurrencyDollarIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, BugAntIcon, CurrencyDollarIcon, CircleStackIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick, useTargetNetwork } from "~~/hooks/scaffold-eth";
 
@@ -32,6 +32,12 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/trade",
     icon: <CurrencyDollarIcon className="h-4 w-4" />
   },
+
+  {
+    label: "Pool",
+    href: "/pool",
+    icon: <CircleStackIcon className="h-4 w-4" />
+  }
 ];
 
 export const HeaderMenuLinks = () => {
